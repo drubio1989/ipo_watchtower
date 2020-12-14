91dd026b08471169950b904ba67c7b9e57a3cc0b
Daniel Rubio <drubio1989@gmail.com>
2020-12-14 12:22:06 -0500

    remove system bundle

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -4,3 +4,5 @@ source /etc/profile.d/rvm.sh
 rvm use 2.7.2
 bundle install --path vendor/bundle
 RAILS_ENV=production bundle exec rails db:migrate
+# Hack fix to remove system bundle
+rm /home/ubuntu/bin/bundle
