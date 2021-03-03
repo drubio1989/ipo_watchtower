01f1cf5015ee6aedc80b87f3ee71909f40ed0d1e
Daniel Rubio <drubio1989@gmail.com>
2021-03-02 20:11:34 -0600

    modify iponotificationgroup mailer to return if no ipos are releasing

--- app/mailers/ipo_notification_mailer.rb
+++ app/mailers/ipo_notification_mailer.rb
@@ -4,11 +4,13 @@ class IpoNotificationMailer < ApplicationMailer
 
   def send_weekly_notification
     @ipos = IpoProfile.includes(company: :stock_ticker).where("DATE(expected_to_trade) >= ?", Date.today.beginning_of_week).order(expected_to_trade: :desc)
+    return if @ipos.empty?
     mail(subject: 'Upcoming Ipos This Week.')
   end
 
   def send_daily_notification
     @ipos = IpoProfile.includes(company: :stock_ticker).where("DATE(expected_to_trade) = ?", Date.today)
+    return if @ipos.empty?
     mail(subject: 'Ipos Trading Today.')
   end
 end
